79133feb1deddd5c1caef7d9c9ebe98fcfe8c704
katie <khartnack@yahoo.com>
2015-05-17 19:30:40 -0700

    adding how to sample

---
+++ DriveProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DriveProject
+//
+//  Created by Dave Beltramini on 5/17/15.
+//  Copyright (c) 2015 Katie Beltramini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GTMOAuth2ViewControllerTouch.h"
+#import "GTLDrive.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriveProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DriveProject
+//
+//  Created by Dave Beltramini on 5/17/15.
+//  Copyright (c) 2015 Katie Beltramini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
